7c0f3c9fb76726aecfe949898fe0dd0259a4db77
Hannah Weaver <hannah@nashvilleprinting.com>
2016-09-06 11:11:53 -0500

    Add project files.

---
+++ WebApplication6/Controllers/MyWeatherController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WebApplication6.Models;
+namespace WebApplication6.Controllers
+{
+    public class MyWeatherController : Controller
+    {
+        // GET: MyWeather
+        public ActionResult Index()
+        {
+            return View();            
+        }
+
+        [HttpPost]
+        public ActionResult GetWeather(FormCollection values)
+        { 
+                using (var client = new WebClient())
+            {
+            //User Input
+               var uCity = values["UserCity"];
+               var uState = values["UserState"];
+            //Pass user entered city and state into zip code api
+                /*  var ZipApi = "http://api.zippopotam.us/us/" + uState + "/" + uCity;
+                string ZipList = clientA.DownloadString(ZipApi);
+                JObject ZipCodes = JObject.Parse(ZipList);
+                //Get first zip code listed for entered city and state
+                string MyZip = (string)ZipCodes["place"][0]["post code"];
+                //Build the url for open weather api using retrieved zip code
+                 public string WeatherApiUri = "http://api.openweathermap.org/data/2.5/weather?zip=" + MyZip + ",us&APPID=170945a535e2a0e23bd4fc35c9f9123a";*/
+                string json = client.DownloadString("http://api.openweathermap.org/data/2.5/weather?zip=33510,us&APPID=170945a535e2a0e23bd4fc35c9f9123a");
+                JObject WeatherReport = JObject.Parse(json);
+                SuperData superData = new SuperData
+                {
+                    temp = (string)WeatherReport["main"]["temp"],
+                    humidity = (string)WeatherReport["main"]["humidity"],
+                    //Find a way to get the chance of rain from the data provided in the current weather data provided
+                    //rainprob = 
+
+                    sunset = (string)WeatherReport["sys"]["sunset"]
+                };
+                //Return Results using a partial view
+                //return PartialView()
+
+
+    }
+            
+        }
+    }
+
+}
---
+++ WebApplication6/Models/MyWeather.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication6.Models
+{
+     public class LocationEntry
+     {
+            public string UserCity { get; set; }
+            public string UserState { get; set; }
+     }
+    
+    public class MyWeather
+    {
+        public int lon { get; set; }
+        public string lat { get; set; }
+    }
+
+    public class SuperData
+    {
+        //public string cName { get; set;  }
+        public string temp { get; set; }
+        public string humidity { get; set; }
+        public string rainprob { get; set; }
+        public string sunset { get; set; }
+    }
+}
